747f1512ae78f516e7311e7677da5716f80de2a0
PanharithSor16 <panharithsor16@gmail.com>
2024-06-25 13:21:58 +0700

    react-pdf-printing

---
+++ src/components/PrintComponent.jsx
@@ -0,0 +1,25 @@
+
+import React, { forwardRef } from 'react';
+import Barcode from 'react-barcode';
+
+const PrintComponent = forwardRef((props, ref) => {
+  return (
+    <div ref={ref} className="p-4 bg-white flex place-content-center">
+     <div className=' flex flex-col place-content-center mt-4 w-full'>
+     <h1 className="text-2xl font-bold mb-2 text-center">07P-RIJ-K-3RL
+      </h1>
+   {/* //   <p>This content will be converted to PDF and printed.</p> */}
+      <Barcode value='0015' height={70} className=" w-full" />
+      <div className=' flex place-content-around'>
+        <div className='mr-4'> SD MC</div>
+        <div className='mr-4'>Connetor</div>
+        <div className='mr-10'>JST</div>
+        <div className='mr-4'>0</div>
+      </div>
+      {/* Add more content as needed */}
+     </div>
+    </div>
+  );
+});
+
+export default PrintComponent;
